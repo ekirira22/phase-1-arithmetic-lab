a6370cefe42036fb356cf90ff6f570c075ce5548
Eric Maranga <ekirira22@gmail.com>
2023-12-12 22:05:57 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+var num1 = 31
+var num2 = 2
+var num3 = 10
+var num4 = 6
 
-// Write your code here
+let multiply = num1 * num2
+let random = Math.ceil((Math.random() * 10))
+let mod = num3 % 6
+let max = Math.max(16,17,18,19,20)
+
+// console.log(max)
